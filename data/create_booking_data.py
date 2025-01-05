ee90b15e9ecbcb12be2cd2304d0a4e0030406d63
Ralina <seitovaralina1@gmail.com>
2025-01-06 00:53:47 +0600

    feature: define more test data to cover all equivalence classes

--- data/create_booking_data.py
+++ data/create_booking_data.py
@@ -13,17 +13,55 @@ valid_data = (
         additionalneeds="Breakfast"
     ),
     BookingModel(
-        firstname="John",
-        lastname="Doe",
-        totalprice=-1,
-        depositpaid=True,
-        checkin=date(2023, 1, 1),
-        checkout=date(2019, 12, 10), 
-        additionalneeds="Breakfast&Lunch"
+        firstname="",                          # поле допускает пустую строку
+        lastname="",                           # поле допускает пустую строку
+        totalprice=-1,                         # поле заменится на null
+        depositpaid="no",                      # поле заменится на true
+        checkin=date(2019, 12, 4),             # checkin позже checkout, нет ошибки (¬_¬ )
+        checkout="2018/01/01 or 2018/01/02",   # поле заменится на 1970-01-01
+        # поле additionalneeds необязательно
     ),
+)
+valid_ids = (
+    'All fields are valid',
+    'All fields contain errors, but they are handled by the API',
 )
 
 invalid_data = (
+    BookingModel(
+        lastname="Doe", 
+        totalprice=100, 
+        depositpaid=True, 
+        checkin=date(2019, 12, 4), 
+        checkout=date(2019, 12, 10)
+    ),
+    BookingModel(
+        firstname="John", 
+        totalprice=100, 
+        depositpaid=True, 
+        checkin=date(2019, 12, 4), 
+        checkout=date(2019, 12, 10)
+    ),
+    BookingModel(
+        firstname="John", 
+        lastname="Doe", 
+        depositpaid=True, 
+        checkin=date(2019, 12, 4), 
+        checkout=date(2019, 12, 10)
+    ),
+    BookingModel(
+        firstname="John", 
+        lastname="Doe", 
+        totalprice=100,  
+        checkin=date(2019, 12, 4), 
+        checkout=date(2019, 12, 10)
+    ),
+    BookingModel(
+        firstname="John", 
+        lastname="Doe", 
+        totalprice=100, 
+        depositpaid=True, 
+    ),
     BookingModel(
         firstname="John",
         lastname="Doe",
@@ -32,11 +70,31 @@ invalid_data = (
         checkin=date(2023, 1, 1),
     ),
     BookingModel(
-        firstname="", 
-        lastname="", 
-        totalprice=0, 
-        checkin=date(2020, 1, 12), 
-        checkout=date(2019, 12, 12), 
-        additionalneeds=""
-    )
+        firstname=123, 
+        lastname="Doe", 
+        totalprice=100, 
+        depositpaid=True, 
+        checkin=date(2025, 1, 1), 
+        checkout=date(2025, 1, 10)
+    ),
+    BookingModel(
+        firstname="John", 
+        lastname=True,
+        totalprice=100,
+        depositpaid=0, 
+        checkin=date(1000, 1, 1),
+        checkout=date(2025, 1, 10),
+    ),
+    BookingModel()
+)
+invalid_ids = (
+    'Missing required parameter: firstname',
+    'Missing required parameter: lastname',
+    'Missing required parameter: totalprice',
+    'Missing required parameter: depositpaid',
+    'Missing required parameter: bookingdates',
+    'Missing required parameter: checkout',
+    'Invalid data type: int in firstname',
+    'Invalid data type: bool in lastname',
+    'All parameters are missing',
 )
--- data/create_token_data.py
+++ data/create_token_data.py
@@ -1,6 +1,28 @@
 from model.token_model import TokenModel
 
 
-valid_data = (TokenModel('admin', 'password123'),)
+valid_data = [TokenModel('admin', 'password123')]
+valid_ids = ['Valid username and password']
 
-invalid_data = (TokenModel('бобр', 'курва'), TokenModel(0, 1))
+invalid_data = [
+    TokenModel('', 'password123'),
+    TokenModel('admin', ''), 
+    TokenModel('invalidUser', 'wrongPassword'),
+    TokenModel('', ''),             
+    TokenModel(0, 1),
+    TokenModel(True, False),       
+    TokenModel(password='password123'),      
+    TokenModel(username='admin'),             
+    TokenModel(),                          
+]
+invalid_ids = [
+    'Empty username',
+    'Empty password',
+    'Both parameters are invalid strings',
+    'Both parameters are empty',
+    'Both parameters are invalid integers',
+    'Both parameters are invalid booleans',
+    'Missing username',
+    'Missing password',
+    'Both parameters are missing',
+]
