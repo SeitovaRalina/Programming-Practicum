d3e5a09c5360c0069c8f1f1505484f25c92bd1c5
Ralina <seitovaralina1@gmail.com>
2025-01-06 12:44:05 +0600

    feature: add post-request with the deletion of a booking and checking its absence

--- test/test_create_booking.py
+++ test/test_create_booking.py
@@ -1,6 +1,7 @@
 import allure
 import pytest
 from api.restful_booker_api import RestfulBookerApi
+from helper.booking_helpers import create_cookie
 from helper.load import load_data
 from model.booking_model import BookingModel
 
@@ -20,11 +21,19 @@ class TestCreateBooking:
     async def test_create_booking_valid_parameters(
         self, restful_booker_api: RestfulBookerApi, request_parameters: BookingModel
     ):
+        cookie = await create_cookie(restful_booker_api)
         response = await restful_booker_api.create_booking(request_parameters)
 
         response.status_code_should_be(200)
         await response.json_schema_should_be_valid('create_booking_schema')
         await response.have_value_in_response_parameter(['booking', 'firstname'], request_parameters.firstname)
+
+        booking_id = await response.get_payload(['bookingid'])
+        response_post = await restful_booker_api.delete_booking(booking_id, cookie)
+        response_post.status_code_should_be(201)
+
+        response_check = await restful_booker_api.get_booking(booking_id)
+        response_check.status_code_should_be(404)
 
     @pytest.mark.parametrize('request_parameters',
                              load_data('create_booking_data', 'invalid_data'),
